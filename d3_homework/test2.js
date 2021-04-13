12d7a121ee3ab689c121157f8ff33dd05597b504
ppak9 <petepp@naver.com>
2021-04-13 22:04:29 +0900

    lebmouse

--- d3_homework/test2.js
+++ d3_homework/test2.js
@@ -13,27 +13,85 @@ function randomExtract(input_array){
     return d3.shuffle(input_array).slice(0,100).sort()
 }
 
-let type1 = document.querySelector("#selectButton");
+let type1 = document.querySelector(".selectXbutton");
+let type2 = document.querySelector(".selectYbutton");
 let xValue;
 let yValue;
 
+let xArray = [];
+let yArray = [];
+
+function randomExtract(input_array){
+    return d3.shuffle(input_array).slice(0,100).sort()
+}
 
 type1.addEventListener('change',(event)=>{
     xValue = event.target.value;
-    let newarray = [];
     data.forEach(function(d){
         switch (xValue){
             case 'rotten_rating':
                 d.rotten_rating = parseFloat(d.rotten_rating);
-                newarray.push(d.rotten_rating);
+                xArray.push(d.rotten_rating);
                 break;
             case 'worldwide_gross':
                 d.worldwide_gross = parseFloat(d.worldwide_gross);
-                newarray.push(d.worldwide_gross);
+                xArray.push(d.worldwide_gross);
+                break;
+            case 'budget':
+                d.budget = parseFloat(d.budget);
+                xArray.push(d.budget);
+                break;
+            case 'us_gross':
+                d.us_gross = parseFloat(d.us_gross);
+                xArray.push(d.us_gross);
+                break;
+            case 'imdb_rating':
+                d.imdb_rating = parseFloat(d.imdb_rating);
+                xArray.push(d.imdb_rating);
+                break;
+            case 'imdb_votes':
+                d.imdb_votes = parseFloat(d.imdb_votes);
+                xArray.push(d.imdb_votes);
                 break;
         }
-
+   
     })
-    console.log(newarray);
 })
+
+type2.addEventListener('change',(event)=>{
+    yValue = event.target.value;
+    data.forEach(function(d){
+        switch (yValue){
+            case 'rotten_rating':
+                d.rotten_rating = parseFloat(d.rotten_rating);
+                yArray.push(d.rotten_rating);
+                break;
+            case 'worldwide_gross':
+                d.worldwide_gross = parseFloat(d.worldwide_gross);
+                yArray.push(d.worldwide_gross);
+                break;
+            case 'budget':
+                d.budget = parseFloat(d.budget);
+                yArray.push(d.budget);
+                break;
+            case 'us_gross':
+                d.us_gross = parseFloat(d.us_gross);
+                yArray.push(d.us_gross);
+                break;
+            case 'imdb_rating':
+                d.imdb_rating = parseFloat(d.imdb_rating);
+                yArray.push(d.imdb_rating);
+                break;
+            case 'imdb_votes':
+                d.imdb_votes = parseFloat(d.imdb_votes);
+                yArray.push(d.imdb_votes);
+                break;
+        }
+    
+    })
+    console.log(yArray);
+    yArray =[];
+
+})
+
 
